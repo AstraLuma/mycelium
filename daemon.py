e888dddcd51f48ff75167a4b51d3126803e6c591
Jamie Bliss <jamie@ivyleav.es>
2020-12-21 20:17:08 -0500

    Starting something useful

--- daemon.py
+++ daemon.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import enum
+import logging
 import time
 
 import board
@@ -7,61 +9,51 @@ import neopixel
 POTS = 12
 PIXELS_PER_POT = 12
 
+logging.basicConfig(
+    level='DEBUG',
+)
+
 
 pixels = neopixel.NeoPixel(board.D18, POTS * PIXELS_PER_POT, brightness=0.3, auto_write=False)
 
 
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b)
+class States(enum.Enum):
+    BLANK = enum.auto()
+    GOOD = enum.auto()
+    BAD = enum.auto()
+    WEIRD = enum.auto()
 
 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(12):
-            pixel_index = (i * 256 // 12) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-        time.sleep(wait)
+COLORS = {
+    States.BLANK: (64, 64, 64),
+    States.GOOD: (0, 255, 0),
+    States.BAD: (255, 0, 0),
+    States.WEIRD: (255, 0, 255),
+}
+
+
+def xmas():
+    for i in range(len(pixels)):
+        pixels[i] = COLORS[States.GOOD if i % 2 else States.BAD]
 
 
 while True:
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((255, 0, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0, 0))
+    pixels.fill(COLORS[States.BLANK])
     pixels.show()
     time.sleep(1)
 
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 255, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0, 0))
+    pixels.fill(COLORS[States.GOOD])
     pixels.show()
     time.sleep(1)
 
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 0, 255))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255, 0))
+    pixels.fill(COLORS[States.BAD])
     pixels.show()
     time.sleep(1)
 
-    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+    pixels.fill(COLORS[States.WEIRD])
+    pixels.show()
+    time.sleep(1)
+
+    xmas()
+    pixels.show()
+    time.sleep(1)
